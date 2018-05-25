311dbe9def089e6dd5013c3bd2e91a3be3eff2b5
naya3919 <moloc@naver.com>
2018-05-25 17:49:57 +0900

    ajax3개 해놨습니다

--- HGL/src/main/webapp/WEB-INF/views/MemberSignUp.jsp
+++ HGL/src/main/webapp/WEB-INF/views/MemberSignUp.jsp
@@ -23,35 +23,30 @@
       <input type="hidden" name="returl" value="" />
 		  <div>
 	   	  	<label>아이디<br><input class="info" type="text" name="userId" title="n" required="required" placeholder="아이디를 입력해주세요"></label>
-	    	<a class="ajChk" id="unId" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 아이디 입니다.</strong></a>
 	      	<a class="ajChk" id="useId" style="color: #3ee625; font-size: 0.8em;"><strong>사용 가능한 아이디 입니다.</strong></a>
+	    	<a class="ajChk" id="unId" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 아이디 입니다.</strong></a>
 	      </div>
 	      <br><div>
-	      	<label>비밀번호<br><input class="info" type="text" name="userPw" onchange="tabBlock();" onclick="idChkConfirm();" 
-	      	required="required" placeholder="비밀번호를 입력해주세요"></label>
+	      	<label>비밀번호<br><input class="info" type="text" name="userPw" required="required" placeholder="비밀번호를 입력해주세요"></label>
 	      </div>
 	      <div>
-	      <br><label>비밀번호 확인<br><input class="info" type="text" name="userChkpw" onchange="tabBlock();" onclick="idChkConfirm();" 
-	      	required="required" placeholder="비밀번호를 다시 입력해주세요"></label>
+	      <br><label>비밀번호 확인<br><input class="info" type="text" name="userChkpw" required="required" placeholder="비밀번호를 다시 입력해주세요"></label>
 	      </div>
 	      <br><div>
-	      	<label>닉네임<br><input class="info" type="text" name="userNick" onchange="tabBlock();" onclick="idChkConfirm();"
-	      	required="required" placeholder="닉네임을 입력해주세요"></label>
-	      	<a class="ajChk" id="unNick" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 닉네임 입니다.</strong></a>
+	      	<label>닉네임<br><input class="info" type="text" name="userNick"	required="required" placeholder="닉네임을 입력해주세요"></label>
 	      	<a class="ajChk" id="useNick" style="color: #3ee625; font-size: 0.8em;"><strong>사용 가능한 닉네임 입니다.</strong></a>
+	      	<a class="ajChk" id="unNick" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 닉네임 입니다.</strong></a>
 	      </div>
 	      <br><div>
-	      	<label>연락처<br><input class="info" type="text" name="userPhone" onchange="tabBlock();" onclick="idChkConfirm();"
-	      	required="required" placeholder="휴대폰번호를 입력해주세요"></label>
+	      	<label>연락처<br><input class="info" type="text" name="userPhone" required="required" placeholder="휴대폰번호를 입력해주세요"></label>
 	      	<p style="margin-top:-2px;"><input class="checkbox" type="checkbox" name="sns" onclick="snsChecked();"><a class="sns" onclick="snsChecked();">문자알림을 받으시겠습니까?</a></p>
 	      </div>
-	      <div>이메일<br>
-	      	<label><input class="info" type="text" name="userEmail" onchange="tabBlock();" onclick="idChkConfirm();"
-	      	required="required" placeholder="이메일을 입력해주세요"></label>
-	      	<a class="ajChk" id="unEmail" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 E-MAIL 입니다.</strong></a>
+	      <div>
+	      	<label>이메일<br><input class="info" type="text" name="userEmail" required="required" placeholder="이메일을 입력해주세요"></label>
 	      	<a class="ajChk" id="useEmail" style="color: #3ee625; font-size: 0.8em;"><strong>사용 가능한 E-MAIL 입니다.</strong></a>
+	      	<a class="ajChk" id="unEmail" style="color: #F15F5F; font-size: 0.8em;"><strong>사용 불가능한 E-MAIL 입니다.</strong></a>
 	      </div>
-	      <br><div>주  소<br>
+	      <br><div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;주  소<br>
 		      <select name="country" onChange="showSub(this.options[this.selectedIndex].value);" style="height: 37px;" required="required">
 		      	<option value="">도를 선택해주세요</option>
 		      	<option value="서울특별시">서울특별시</option>
@@ -350,7 +345,7 @@
 		      
 	      <br><input class="addrDetail" type="text" name="userAddr" required="required" placeholder="상세주소를 입력해주세요" style="vertical-align: 1px;">
 	      </div>
-	      <div style="margin-top: 10%; width:370px;" align="center">
+	      <div style="margin-left: 10%; margin-top: 10%; width:370px;" align="center">
 	      <input type="submit" value="가입하기">
 	      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 	      <input type="button" value="취소하기"> 
--- HGL/src/main/webapp/js/MemberSignUp.js
+++ HGL/src/main/webapp/js/MemberSignUp.js
@@ -1,35 +1,10 @@
-   function idChk() { // 중복체크
-      var doc = document.getElementsByName("userId")[0];
-      if (doc.value == null || doc.value == "") {
-         alert("ID를 입력해주세요")
-      } else {
-         open("TrioController?command=idChk&userId=" + doc.value); // open : 새창을 띄어줌. 
-      }
-   }
-   
-   function idChkConfirm() { // id중복체크를를 우선적으로 하게 하는 기능.
+/*   function idChkConfirm() { // id중복체크를를 우선적으로 하게 하는 기능.
       var chk = document.getElementsByName("userId")[0].title;
       if (chk == "n") {
          alert("아이디 중복체크를 해주세요");
          document.getElementsByName("userId")[0].focus();
       }
-   }
-   
-   function tabBlock(){ //탭 이동 막기
-	      var chk = document.getElementsByName("userId")[0].title;
-	         if (chk == "n") {
-	            alert("아이디 중복체크를 해주세요");
-	            document.getElementsByName("userId")[0].focus();
-	            document.getElementsByName("userId")[0].value="";
-	            document.getElementsByName("userPw")[0].value="";
-	            document.getElementsByName("userChkpw")[0].value="";
-	            document.getElementsByName("userNick")[0].value="";
-	            document.getElementsByName("userPhone")[0].value="";
-	            document.getElementsByName("userEmail")[0].value="";
-	            document.getElementsByName("userAddr")[0].value="";
-	         }
-	}
-
+   }*/
 	function snsChecked() { //문자알림서비스 확인창
 		var retVal = confirm("문자알림을 받으시겠습니까?\n매달 1일, 부모님께 안부를 물어보도록 유도하는 알림문자가 발송되며,\n캘린더에 일정 추가 시 일정 알림을 체크하면 \n해당 일정 날짜의 하루 전에 알림 문자가 발송됩니다.");
 
@@ -351,23 +326,63 @@
 				break;
 		}
    }
+	
 	$(function(){
-		      $("input[name=userId").on("change",function() {
-		            var idVal = $("input[name=userId]").val();
-		            alert(idVal);
-		            $.ajax({
-		               type:"post",
-		               url : ".do",
-		               date:"useId="+idVal,
-		               async : false,
-		               success : function(res) {
-		                  var r = $.trim(res)
-		                  if (r == "t") {
-		                     $("input[name=useId]").prop("display", "inline");
-		                  }else{
-		                	 $("input[name=unId]").prop("display", "inline");
-		                  }
-		               }
-		            });
-		         })
-		       })
+      $("input[name=userId").on("change",function() {
+            var idVal = $("input[name=userId]").val();
+            alert(idVal);
+            $.ajax({
+               type:"post",
+               url : ".do",
+               date:"useId="+idVal,
+               async : false,
+               success : function(res) {
+                  var r = $.trim(res)
+                  if (r == "t") {
+                     $("input[name=useId]").prop("display", "inline");
+                  }else{
+                	 $("input[name=unId]").prop("display", "inline");
+                  }
+               }
+            });
+         })
+
+		     
+	  $("input[name=userNick").on("change",function() {
+	        var nickVal = $("input[name=userNick]").val();
+	        alert(nickVal);
+	        $.ajax({
+	           type:"post",
+	           url : ".do",
+	           date:"nick="+nickVal,
+	           async : false,
+	           success : function(res) {
+	              var r = $.trim(res)
+	              if (r == "t") {
+	                 $("input[name=useNick]").prop("display", "inline");
+	              }else{
+	            	 $("input[name=unNick]").prop("display", "inline");
+	              }
+	           }
+	        });
+	     })
+	
+	  $("input[name=userEmail").on("change",function() {
+	        var emailVal = $("input[name=userEmail]").val();
+	        alert(emailVal);
+	        $.ajax({
+	           type:"post",
+	           url : ".do",
+	           date:"email="+emailVal,
+	           async : false,
+	           success : function(res) {
+	              var r = $.trim(res)
+	              if (r == "t") {
+	                 $("input[name=useEmail]").prop("display", "inline");
+	              }else{
+	            	 $("input[name=unEmail]").prop("display", "inline");
+	              }
+	           }
+	        });
+	     })	       
+	})
