de0d22b1acb79abb6fa09b1cb52d82fbe620904b
HyangEun <37951290+giddms42@users.noreply.github.com>
2018-05-25 14:10:12 +0900

    내꺼까지 올리기

--- HGL/src/main/java/com/lol/hgl/controller/MemberController.java
+++ HGL/src/main/java/com/lol/hgl/controller/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MemberController {
 	
-	@RequestMapping(value = "MemberSignUp.do")
+	@RequestMapping(value = "MemberSignUpForm.do")
 	public String memberSignUpform() {	
 		return "MemberSignUp";
 	}
